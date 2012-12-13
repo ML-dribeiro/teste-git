78da16c4928298173032fa0e1bad128f6da65c4e
Wesley Cintra do Nascimento <wesley.nascimento@mercadolivre.com>
2012-12-13 10:21:08 -0200

    incluindo o index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$toJSON = array("cep_origem"=> "88040000",
+"cep_destino"=> "04050000",
+"valor"=> "1000,00",
+"qtd_volumes"=>"1",
+"quantidade[0]"=>"1",
+"comprimento[0]"=>"10",
+"largura[0]"=>"20",
+"altura[0]"=>"10",
+"peso[0]"=>"0,500",
+"setor[0]"=>"22"
+);
+
+	
+echo json_encode($toJSON);

78d500308366d9861126c4bb1b9079945af177e7
Daiane <dribeiro@DRIBEIRO.ml.com>
2012-12-13 10:31:04 -0200

    correcao do index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $toJSON = array("cep_origem"=> "88040000",
 "cep_destino"=> "04050000",
-"valor"=> "1000,00",
+"valor"=> "10,00",
 "qtd_volumes"=>"1",
 "quantidade[0]"=>"1",
 "comprimento[0]"=>"10",
